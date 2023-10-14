e36959c4e4eb2bbe94562d87fbdc00b47394a9a5
Lyubomir Ternavskiy <127299159+LyubomirT@users.noreply.github.com>
2023-10-14 21:16:01 +0300

    Update codeComments.js

--- src/codeComments.js
+++ src/codeComments.js
@@ -1,8 +1,17 @@
-function parseCodeComments(code) {
-    const comments = code.match(/\/\/.*|\/\*[\s\S]*?\*\//g);
+function parseCodeComments(code, language) {
+    let commentSyntax;
+
+    switch (language) {
+        case 'javascript':
+            commentSyntax = /\/\/.*|\/\*[\s\S]*?\*\//g;
+            break;
+        case 'python':
+            commentSyntax = /#.*|\'\'\'[\s\S]*?\'\'\'|\"\"\"[\s\S]*?\"\"\"/g;
+            break;
+        default:
+            throw new Error(`Unsupported language: ${language}`);
+    }
+
+    const comments = code.match(commentSyntax);
     return comments || [];
 }
-
-module.exports = {
-    parseCodeComments
-};
